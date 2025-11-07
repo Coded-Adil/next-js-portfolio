ad050e4ca107531fe4b73c9ab465775bcd9b905a
Coded-Adil <itsadil2022@gmail.com>
2025-11-07 20:22:15 +0600

    Implemented a responsive Services section and code review

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import Header from "@/components/Header";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       {/* <Navbar /> */}
       <Header />
       <About />
+      <Services />
     </>
   );
 }
